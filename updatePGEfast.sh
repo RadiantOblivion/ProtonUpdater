abe3ce527a9ef59c3107ff5a616f7472730df47e
RadiantOblivion <89446269+RadiantOblivion@users.noreply.github.com>
2023-05-05 15:15:35 -0400

    Update updatePGEfast.sh

--- updatePGEfast.sh
+++ updatePGEfast.sh
@@ -3,17 +3,25 @@ baseuri="https://github.com/GloriousEggroll/proton-ge-custom/releases/download"
 latesturi="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
 dstpath="$HOME/.steam/root/compatibilitytools.d"
 
+RestartSteam() {
+  if [ "$( pgrep steam )" != "" ]; then
+    echo "Restarting Steam"
+    pkill -TERM steam #restarting Steam
+    sleep 5s
+    nohup steam </dev/null &>/dev/null &
+  fi
+}
 
   latestversion="$(curl -s $latesturi | grep -E -m1 "tag_name" | cut -d \" -f4)"
   if [[ -d $dstpath/$latestversion ]]
   then
-    echo "Version $latestversion is the latest version and is already installed."
+    echo "$latestversion is the latest version and is already installed."
     sleep 1
     echo "Exiting..."
     sleep 1
     exit 0
   else
-    echo "Version $latestversion is the latest version and is not installed yet."
+    echo "$latestversion is the latest version and is not installed yet."
     sleep 3
     echo "Installing the latest version of Proton now!"
     sleep 2
@@ -34,12 +42,3 @@ echo "$rsp" | grep -q 302 || {
 curl -sL "$url" | tar xfzv - -C "$dstpath"
 
 RestartSteam
-
-RestartSteam() {
-  if [ "$( pgrep steam )" != "" ]; then
-    echo "Restarting Steam"
-    pkill -TERM steam #restarting Steam
-    sleep 5s
-    nohup steam </dev/null &>/dev/null &
-  fi
-}
